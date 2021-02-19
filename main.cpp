96387f450744f5b83267af5df6a3c8ebf11092e4
maxime haselbauer <m.haselbauer@luminartech.com>
2021-02-19 08:03:38 +0100

    Clean master

--- main.cpp
+++ main.cpp
@@ -14,27 +14,8 @@
 #include <tuple>
 #include <vector>
 
-#include <modules.h>
-
-
-
-// void wireTogether(std::pair<std)
-
-
-// connect(OneA,
-
 int main() {
 
-  PodOne pod_one;
-  pod_one.timestamp=-12;
-
-  Two two;
-  PodOne pod;
-  std::cout<<pod.isValid();
-
-  // std::function<void (Two,const PodTwo)> callback=&Two::triggerTwo;
-  OneA one_a(two,&Two::triggerTwo);
-  one_a.setPod(PodOne{});
 
 
   return 0;
