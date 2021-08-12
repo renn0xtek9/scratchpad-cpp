c7fff96b26c28ed9de277922d305323d53b9ed50
Maxime Haselbauer <maxime.haselbauer@luminartech.com>
2021-08-12 11:50:55 +0200

    example of private inheritance

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <condition_variable>
 #include <exception>
 #include <functional>
+#include <future>
 #include <iostream>
 #include <limits>
 #include <memory>
@@ -14,9 +15,39 @@
 #include <tuple>
 #include <vector>
 
+using namespace std::literals::chrono_literals;
+
+class LocalPoseCalculation {
+public:
+  LocalPoseCalculation() = default;
+  virtual ~LocalPoseCalculation() = default;
+
+  LocalPoseCalculation(const LocalPoseCalculation &) = default;
+  LocalPoseCalculation(LocalPoseCalculation &&) = default;
+
+  LocalPoseCalculation &operator=(const LocalPoseCalculation &) = default;
+  LocalPoseCalculation &operator=(LocalPoseCalculation &&) = default;
+
+  virtual void updateAccelerationAndAngularRate(const int foo) = 0;
+};
+
+class LocalPoseCalculationFromGlobalPose final : public LocalPoseCalculation {
+public:
+  explicit LocalPoseCalculationFromGlobalPose(){};
+
+private:
+  void updateAccelerationAndAngularRate(const int foo) override{};
+};
+
 int main() {
 
+  std::unique_ptr<LocalPoseCalculation> local_pose_calc_ =
+      std::make_unique<LocalPoseCalculationFromGlobalPose>();
 
+  //Uncommenting the later won't compile
+  // local_pose_calc_->updateAccelerationAndAngularRate();
+
+  std::cout << "End" << std::endl;
 
   return 0;
 }
