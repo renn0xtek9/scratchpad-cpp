d059a7a030fd19106c5691169fada8a3cdef6154
renn0xtek9 <renn0xtek9@laposte.net>
2025-04-22 14:41:44 +0200

    example of how to enforce maximum limit on process with cgroup

--- main.cpp
+++ main.cpp
@@ -14,16 +14,26 @@
 #include <thread>
 #include <tuple>
 #include <vector>
+#include <fstream>
 
 #include "dummy_classes.h"
 
 int main()
 {
-  StringRuleOfFive item;
-  item.print();
+  std::size_t number_of_bytes = 1024U * 1024U;
 
-  std::cout
-      << "End" << std::endl;
+  std::cout << "This programm will allocate " << number_of_bytes << " bytes." << std::endl;
+
+  std::vector<char> vector{};
+  vector.reserve(number_of_bytes);
+
+  for (int i = 0; i < number_of_bytes; ++i)
+  {
+    std::cout << "Allocating byte no: " << i << " out of " << number_of_bytes << std::endl;
+    vector.push_back('a');
+  }
+
+  std::cout << "Success !!" << std::endl;
 
   return 0;
 }
---
+++ run_the_example.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+CGROUPNAME="scratchpad"
+BYTES="1048576" # 1MB
+echo "This will create the cgroup $CGROUPNAME and set its maximum memory limit to $BYTES bytes"
+sudo cgcreate -g memory:"$CGROUPNAME"
+echo "$BYTES" | sudo tee /sys/fs/cgroup/memory/"$CGROUPNAME"/memory.limit_in_bytes
+echo "$BYTES" | sudo tee /sys/fs/cgroup/memory/"$CGROUPNAME"/memory.kmem.limit_in_bytes
+echo "$BYTES" | sudo tee /sys/fs/cgroup/memory/"$CGROUPNAME"/memory.memsw.limit_in_bytes
+
+# Give ownership to user to allow starting cgexex for this group without sudo
+sudo chown -R "$USER" /sys/fs/cgroup/memory/"$CGROUPNAME"
+
+# Build the executable
+cmake --B build 
+cmake --build build
+
+# Runit with cgexec to enforce the memory limit
+cgexec -g memory:scratchpad $(pwd)/build/main
