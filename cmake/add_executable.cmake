70fe54c86165cb3fcadbb7afdf0403a5a8ca8508
renn0xtek9 <renn0xtek9@laposte.net>
2025-04-23 09:52:06 +0200

    my_add_executable function

---
+++ cmake/add_executable.cmake
@@ -0,0 +1,32 @@
+function(my_add_executable)
+
+  cmake_parse_arguments(
+    arg # prefix of output variables i.e. arguments will be retrieved as
+    # ARG_FOO ARG_BAR
+    "" # ; separated list of names of boolean arguments (only defined ones will
+       # be true)
+    "CPP_STANDARD" # ; separated list of names of mono-valued arguments
+    "" # ; separated list of names of multi-valued arguments
+    ${ARGN} # arguments of the function to parse, here we take the all original
+    # ones
+  )
+
+  if(NOT arg_CPP_STANDARD)
+    set(arg_CPP_STANDARD 14)
+  endif(NOT arg_CPP_STANDARD)
+
+  set(CMAKE_CXX_STANDARD ${arg_CPP_STANDARD})
+  set(CMAKE_CXX_STANDARD_REQUIRED True)
+  get_filename_component(DIR_NAME ${CMAKE_CURRENT_LIST_DIR} NAME)
+  set(EXECUTABLE_NAME ${DIR_NAME})
+
+  add_executable(${EXECUTABLE_NAME} main.cpp)
+  target_link_libraries(${EXECUTABLE_NAME} pthread)
+
+  install(
+    TARGETS ${EXECUTABLE_NAME}
+    RUNTIME DESTINATION bin
+    LIBRARY DESTINATION lib
+    ARCHIVE DESTINATION lib)
+
+endfunction()
