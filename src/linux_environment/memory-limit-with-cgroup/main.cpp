90816ed2616827ed2f59466cfa2e339db03fad3b
renn0xtek9 <renn0xtek9@laposte.net>
2025-06-27 09:14:16 +0000

    Folder structure change: create src/linux_environment
    Relevant for all c++ code, tips and tricks specific to Linux environments.

--- src/linux_environment/memory-limit-with-cgroup/main.cpp
+++ src/linux_environment/memory-limit-with-cgroup/main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <vector>
 
-int main()
-{
+int main() {
   std::size_t number_of_bytes = 1024U * 1024U;
 
   std::cout << "This programm will allocate " << number_of_bytes << " bytes." << std::endl;
@@ -10,8 +9,7 @@ int main()
   std::vector<char> vector{};
   vector.reserve(number_of_bytes);
 
-  for (int i = 0; i < number_of_bytes; ++i)
-  {
+  for (int i = 0; i < number_of_bytes; ++i) {
     std::cout << "Allocating byte no: " << i << " out of " << number_of_bytes << std::endl;
     vector.push_back('a');
   }
