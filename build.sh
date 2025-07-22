03f2551b580eb68601b8e4ae71449617116f5916
renn0xtek9 <renn0xtek9@laposte.net>
2025-07-22 07:02:24 +0000

    Clean repository

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-# PATH="$PATH:/opt/cmake-4.0.1-linux-x86_64/bin/"
-# export PATH
 set -euxo pipefail
-# rm -rf build
 
 cmake --preset release-ninja -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 cmake --build --preset release-ninja -- -j 20 
