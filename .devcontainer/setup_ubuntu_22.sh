ff5ed62e792c3cc16342a677ad5d5ea008f4afbb
renn0xtek9 <renn0xtek9@laposte.net>
2025-07-22 09:41:30 +0000

    Refactor devcontainer setup script

--- .devcontainer/setup_ubuntu_22.sh
+++ .devcontainer/setup_ubuntu_22.sh
@@ -5,12 +5,27 @@ DEBIAN_FRONTEND=noninteractive
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 sudo apt-get update
-sudo apt-get install -y curl nano gcc lcov gcovr python3 python3-pip git valgrind ninja-build clang-format clang-tidy cpplint cppcheck iwyu shellcheck socat doxygen graphviz makefile2graph plantuml
+
+# Linting tools etc.
+sudo apt-get install -y clang-format clang-tidy cpplint cppcheck iwyu shellcheck
+# Utilities 
+sudo apt-get install -y curl wget nano socat git
+# Compilers build systems and static analysis tools
+sudo apt-get install -y valgrind gcc lcov gcovr ninja-build
+# Python 3 
+sudo apt-get install -y python3 python3-pip
+# Documentation tools
+sudo apt-get install -y doxygen graphviz makefile2graph plantuml
+# Qt6
+sudo apt-get install -y qt6-base-dev qt6-base-dev qt6-3d-dev libgl-dev libopengl-dev
+
+# Install CMake 4.0.1
 cd /tmp/
 wget https://github.com/Kitware/CMake/releases/download/v4.0.1/cmake-4.0.1-linux-x86_64.tar.gz
 sudo tar -xf cmake-4.0.1-linux-x86_64.tar.gz -C /opt/
 rm cmake-4.0.1-linux-x86_64.tar.gz
 
+# Github CLI
 type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
@@ -18,8 +33,6 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githu
 sudo apt update
 sudo apt install gh -y
 
+
+# Install Python packages
 pip3 install pre-commit cmake-format
-
-
-# Install Qt6 
-sudo apt-get install -y qt6-base-dev qt6-base-dev qt6-3d-dev libgl-dev libopengl-dev
